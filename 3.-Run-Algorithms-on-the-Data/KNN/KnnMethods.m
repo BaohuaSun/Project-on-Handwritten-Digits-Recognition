641690777572e0eb3ee6d6d68811734e20a3c4fa
Baohua Sun <BaohuaSun@users.noreply.github.com>
2015-06-05 15:43:30 -0700

    Create KnnMethods.m

---
+++ 3.-Run-Algorithms-on-the-Data/KNN/KnnMethods.m
@@ -0,0 +1,26 @@
+% K-NN
+% Copyright By Baohua Sun
+% 20120409
+close all
+clear all
+clc
+
+%% Training
+% [imData imLabel]=fun_ReadOriginalData('TrainData');
+load TrainDataMatFile.mat
+TrainingData=reshape(imData,size(imData,3),1024);
+Group = imLabel;
+% KNN
+ClassTraining = knnclassify(TrainingData, TrainingData, Group);
+errorTrainingData = sum((ClassTraining-imLabel)~=0)
+errorRateTrainingData = errorTrainingData / size(TrainingData,1)
+
+%% Testing
+% [imData imLabel]=fun_ReadOriginalData('TestData');
+clear imData imLabel
+load TestDataMatFile.mat
+TestingData=reshape(imData,size(imData,3),1024);
+% KNN
+Class = knnclassify(TestingData, TrainingData, Group);
+errorTestingData = sum((Class-imLabel)~=0)
+errorRateTestingData = errorTestingData / size(TestingData,1)
